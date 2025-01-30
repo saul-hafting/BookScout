84ed6c7ed32c23e69672b98e001573956cbb6370
Saul Hafting <saulhafting@gmail.com>
2025-01-30 19:04:17 -0400

    built a responcive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Click me</Button>
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      {useBreakpointValue({ base: null, lg: (
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      )})}
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
